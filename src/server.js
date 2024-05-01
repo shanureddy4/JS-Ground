2ec06e099490d4b8e59665170897463b9e18dfc0
bogathi reddy <bogathi.reddy@powerschool.com>
2024-05-01 16:47:50 +0530

    middleware

--- src/server.js
+++ src/server.js
@@ -4,10 +4,26 @@ import http from 'http'
 
 const PORT = process.env.PORT;
 
+const logger = (req,res,next) =>{
+    console.log(`${req.method} ${req.url}`)
+    next();
+}
+
 const server = http.createServer((req,res)=>{
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
-    res.end('<h1> Hello World </h1>')
+    logger(req,res,()=>{
+        if(req.method === 'GET'){
+            res.setHeader('Content-Type', 'text/html');
+            res.statusCode = 200;
+            res.end('<h1> Hello World </h1>')
+        }
+        else{
+            res.setHeader('Content-Type', 'text/html');
+            res.statusCode = 400;
+            res.end('<h1>only GET supported')
+        }
+
+    })
+    
 })
 
 server.listen(PORT,()=>{
