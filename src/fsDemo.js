8ef30fd74e2117966b60c589aca1f641f632147e
bogathi reddy <bogathi.reddy@powerschool.com>
2024-05-01 17:36:11 +0530

    writing and appending data to the file.

--- src/fsDemo.js
+++ src/fsDemo.js
@@ -1,15 +1,15 @@
-import fs, { readFileSync } from 'fs';
-// import fs from 'fs/promises'
+//import fs, { readFileSync } from 'fs';
+ import fs from 'fs/promises'
 
 //asynchronous way
-fs.readFile('src/textfile.txt','utf-8', (err,data)=>{
-    if(err) throw err;
-    console.log(data);
-})
+// fs.readFile('src/textfile.txt','utf-8', (err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
+// })
 
 //synchronous way
-const data = fs.readFileSync('src/textfile.txt','utf-8');
-console.log(data);
+// const data = fs.readFileSync('src/textfile.txt','utf-8');
+// console.log(data);
 
 //with promises
 // fs.readFile('src/textfile.txt','utf8')
@@ -20,10 +20,29 @@ console.log(data);
 //with async await. 
 const getData = async ()=>{
     try{
-        const data = fs.readFileSync('src/textfile.txt','utf-8');
+        const data = await fs.readFile('src/textfile.txt','utf-8');
         console.log(data);
     }catch(err){
         console.log(err);
     }
 } 
+
+const writeData = async () => {
+    try{
+        await fs.writeFile('./src/textfile.txt','Writing hello world to this file...');
+        console.log("writing in progress");
+
+    }catch(err){
+        console.log(err)
+    }
+}
+const appendSomething = async () =>{
+    try{
+        await fs.appendFile('./src/textfile.txt','\nappending to this file');
+    }catch(err){
+        console.log(err);
+    }
+}
+writeData();
+appendSomething();
 getData();
