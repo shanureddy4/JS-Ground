7b396e82ffbb66302c3d5243120c43d921e69a90
bogathi reddy <bogathi.reddy@powerschool.com>
2024-05-01 14:18:53 +0530

    initial commit : till CommonJs

---
+++ src/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const {meterToCentimeter, feetToCm} = require('./utils')
+console.log(`Meter to centemeter : ${meterToCentimeter(2)}`)
+console.log(`Feet to cm : ${feetToCm(2)}`)
+
+
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+
+function meterToCentimeter(n){
+    return n*1000;
+   }
+function feetToCm(n){
+return n*6;
+}
+   
+   module.exports = {meterToCentimeter, feetToCm}
