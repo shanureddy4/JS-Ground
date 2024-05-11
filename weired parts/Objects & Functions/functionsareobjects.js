69f525df25465fd1e449f895e2ca2e0a03c8fb25
bogathi reddy <bogathi.reddy@powerschool.com>
2024-05-11 15:46:24 +0530

    functions are objects

---
+++ weired parts/Objects & Functions/functionsareobjects.js
@@ -0,0 +1,6 @@
+function greet(){// greet is name of the function
+    console.log("Hi function") // this is code property of the function object.
+}
+greet.someval = "somthing";
+console.log(greet);
+console.log(greet.someval);
