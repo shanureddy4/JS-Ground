83aac60c6a2ccd9e036b6ad1ff64da5d45cb0dd1
bogathi reddy <bogathi.reddy@powerschool.com>
2024-05-13 09:54:05 +0530

    automatic semicolon

---
+++ weired parts/Objects & Functions/automaticsemicolon.js
@@ -0,0 +1,15 @@
+// this is dangerous and funny at a time.
+// function greet(){
+//     return
+//     {
+//         name:"shanu"
+//     }
+// }
+
+function greet(){
+    return{
+        name:"shanu"
+    }
+}
+
+console.log(greet());
