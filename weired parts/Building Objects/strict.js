085ebab71d5389aec08cf55ddfa1a44e0a89c7ca
bogathi reddy <bogathi.reddy@powerschool.com>
2024-05-21 17:48:47 +0530

    strict mode.

---
+++ weired parts/Building Objects/strict.js
@@ -0,0 +1,12 @@
+//'use strict'
+var person;
+persom = {shanu:"reddy"}
+console.log(persom);
+
+function fn (){
+    'use strict'
+    var person2;
+    persom2 = {shanu:"reddy"}
+    console.log(persom2);
+}
+fn();
